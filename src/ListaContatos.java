02ff58caefc8a5702f1f6fc68a50d3ac689fe030
Brenno Oliveria <118616498+Ubrennz@users.noreply.github.com>
2025-03-18 23:35:10 +0000

    Fazendo uma agenda de contatos

---
+++ src/ListaContatos.java
@@ -0,0 +1,11 @@
+import util.Contatos;
+import java.util.Scanner;
+
+public class ListaContatos {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Contatos contato;
+
+        sc.close();
+    }
+}
