34a38943a2d45a4ae12f0414936ddafacf07b50f
Brenno Oliveria <118616498+Ubrennz@users.noreply.github.com>
2025-03-22 00:53:34 +0000

    update ListaContatos

--- src/ListaContatos.java
+++ src/ListaContatos.java
@@ -1,11 +1,40 @@
 import util.Contatos;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class ListaContatos {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Contatos contato;
+    private static Scanner sc = new Scanner(System.in);
+    private static Contatos contato;
+    private static List<Contatos> agendaContatos = new ArrayList<>();
 
-        sc.close();
+
+    public static void criarContato() {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("SISTEMA DE AGENDA DE CONTATOS");
+        int opcao;
+
+        do {
+            System.out.println("[1] para criar um novo contato\n[2] para visualizar a agenda de contatos");
+            System.out.println("[3] para editar um contato\n[4] para remover um contato da agenda de contatos");
+            System.out.println("[0] para sair\nDigite a opção:");
+            opcao = sc.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    criarContato();
+                    break;
+                case 2:
+                    System.out.println();
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
+            }
+        } while (opcao != 0);
+
     }
 }
