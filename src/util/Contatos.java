fbff6f9537de712500bdea0d570a2c5b5b6cee43
Brenno Oliveria <118616498+Ubrennz@users.noreply.github.com>
2025-03-22 00:54:17 +0000

    Classe da agenda de contatos

---
+++ src/util/Contatos.java
@@ -0,0 +1,27 @@
+package util;
+
+public class Contatos {
+    private String nome;
+    private String telefone;
+
+    public Contatos(String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String novoNome) {
+        this.nome = novoNome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String novoTelefone) {
+        this.telefone = novoTelefone;
+    }
+}
